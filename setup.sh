d8e4ef33e8760d30ef6c463c6a68c201b8e2ed5b
Tom Pollak <tompollak1000@gmail.com>
2024-11-01 18:12:53 +0000

    setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/tom-pollak/crosscoder-model-diff-replication.git
+cd crosscoder-model-diff-replication
+pip install -r requirements.txt
+
+huggingface-cli login
+
+huggingface-cli download --repo-type model google/gemma-2b
+huggingface-cli download --repo-type model tommyp111/gemma-2b-clip-lora-golden-gate-all-kr2e_3
+huggingface-cli download --repo-type dataset tommyp111/gg-fineweb-mix-tokenized-gemma2-2b
